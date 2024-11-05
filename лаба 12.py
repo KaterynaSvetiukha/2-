735447853296c00f6101ba19c78fa50d35ab7ce2
Kateryna Svetiukha <148003224+KaterynaSvetiukha@users.noreply.github.com>
2024-11-05 22:51:09 +0200

    Update лаба 12.py

--- лаба 12.py
+++ лаба 12.py
@@ -1,7 +1,7 @@
 import json
 
 
-students = [{"Last name" : "Svetiukha", "Grades" : [8, 9, 10, 7, 8]},
+data = {"students": [{"Last name" : "Svetiukha", "Grades" : [8, 9, 10, 7, 8]},
             {"Last name" : "Kropyvnytskyi", "Grades" : [4, 10, 11, 12, 5]},
             {"Last name" : "Mykytenko", "Grades" : [8, 7, 9, 9, 10]},
             {"Last name" : "Belik", "Grades" : [1, 5, 8, 3, 12]},
@@ -10,12 +10,12 @@ students = [{"Last name" : "Svetiukha", "Grades" : [8, 9, 10, 7, 8]},
             {"Last name" : "Dvornik", "Grades" : [3, 9, 8, 7, 10]},
             {"Last name" : "Elnikova", "Grades" : [9, 12, 6, 8, 9]},
             {"Last name" : "Kyrychenko", "Grades" : [10, 12, 11, 9, 9]},
-            {"Last name" : "Kyslyi", "Grades" : [2, 6, 8, 1, 12]}]
+            {"Last name" : "Kyslyi", "Grades" : [2, 6, 8, 1, 12]}]}
 
-jsonData = json.dumps(students)
+jsonData = json.dumps(data)
 with open("data.json", "wt") as file:
     file.write(jsonData)
-file.close
+file.close()
 
 while True:    
     print("Select an option:\n 1 - Add data\n 2 - View data\n 3 - Find students with max or min summa of the grades\n 4 - Exit") 
@@ -42,29 +42,29 @@ while True:
             with open("data.json", "wt") as file:
                 file.write(jsonData)
 
-    if x == 2:
+    elif x == 2:
         with open("data.json", "rt") as file:
             students = json.loads(jsonData)
             print(*students, sep='\n')
 
-    if x == 3:
+    elif x == 3:
         with open("data.json", "rt") as file:
-            students = json.loads(jsonData)
+            data = json.loads(jsonData)
+            students = data["students"]
             b = max(students, key=lambda student: sum(student['Grades']))
             print(f"Учень з найбільшою сумою оцінок: {b['Last name']} ({sum(b['Grades'])})")
-            data = [{'Last name' : b['Last name']}, {'Grades' : sum(b['Grades'])}]
-            jsond = json.dumps(data)
-            with open("data_2.json", "wt") as file:
-                file.write(jsond)
-            file.close
+            maxdata = [{'Last name' : b['Last name']}, {'Grades' : sum(b['Grades'])}]
 
             w = min(students, key=lambda student: sum(student['Grades']))
             print(f"Учень з найменшою сумою оцінок: {w['Last name']} ({sum(w['Grades'])})")
-            data = [{'Last name' : w['Last name']}, {'Grades' : sum(w['Grades'])}]
-            jsond = json.dumps(data)
-            with open("data_2.json", "at") as file:
-                file.write(jsond)
-            file.close
+            mindata = [{'Last name' : w['Last name']}, {'Grades' : sum(w['Grades'])}]
+        
+            result = {
+                "max_sum_student": maxdata,
+                "min_sum_student": mindata
+            }
+            with open("data_2.json", "wt") as file:
+                json.dump(result, file)
 
-    if x == 4:
+    elif x == 4:
         quit(0)
